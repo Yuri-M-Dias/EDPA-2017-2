0d37d03816f77b3f81b4bf91118bc7e5d7b44323
Yuri-M-Dias <yurimathe.yp@gmail.com>
2017-11-26 13:03:47 -0200

    Slighly changing CMakeLists example to use multiple targets

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <ctime>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <climits>
+
+using namespace std;
+
+int geraNumeroRandomico();
+
+int main() {
+    int n;
+    cout << "Insira o valor de n:" << endl;
+    cin >> n;
+
+    int A[n];
+    clock_t start = clock();
+
+    for (int i = 0; i < n; i++) {
+        int r = geraNumeroRandomico();
+        A[i] = r;
+    }
+
+    clock_t end = clock();
+
+    // CLOCKS_PER_SEC == 10^6 = 1 microsecond
+    double microsecondsTotal = difftime(end, start);
+    double millisecondsTotal = microsecondsTotal / 1000;
+    double secondsTotal = microsecondsTotal / double(CLOCKS_PER_SEC);
+
+    cout << "Execucao em microsegundos: " << microsecondsTotal << endl;
+    cout << "Execucao em millisegundos: " << millisecondsTotal << endl;
+    cout << "Execucao em segundos: " << secondsTotal << endl;
+
+    return 0;
+}
+
+int geraNumeroRandomico() {
+    this_thread::sleep_for(chrono::milliseconds(1));
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator(seed);
+    uniform_int_distribution<int> distribution(INT_MIN, INT_MAX);
+
+    return distribution(generator);
+}
+
